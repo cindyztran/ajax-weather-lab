55b38b0d3e2945d4257255568d6e68ab95d6bc3a
cindytran <cindyztran@gmail.com>
2021-05-20 20:38:32 -0700

    second commit

--- js/script.js
+++ js/script.js
@@ -4,32 +4,35 @@
     const $weatherFor = $('#weatherFor');
     const $temperature = $('#temperature');
     const $feelsLike = $('#feelsLike');
-    
-    const $getWeather = $('#getWeather');
-
+    const $weatherDescription = $('#weatherDescription')
+  
     function render() {
-        $weatherFor.text = (data["sys"]["name"]);
-            $temperature.text =(data["main"]["temp"]);
-            $feelsLike = $(data["main"]["feels_like"]);
-    }
-    function handleGetData() {
+        $weatherFor.text(weatherData['name']);
+        $temperature.text(Math.round(weatherData['main']['temp'])).append('°F');
+        $feelsLike.text(Math.round(weatherData['main']['feels_like'])).append('°F');
+        $weatherDescription.text(weatherData['weather'][0]['description']);
+
+
+    };
+
+    function handleGetData(event) {
         event.preventDefault();
+        const $searchCity = $('input#search').val();
+    
+        $.ajax ({
+            url: `https://api.openweathermap.org/data/2.5/weather?q=${$searchCity}&units=imperial&APPID=96179a5fe80fda98d6fccc841db6e2bc`
+        })
+        .then(
+            function(data) {
+                weatherData = data;
+                render();
+                
+    
+            }, 
+            function(error) {
+                console.log('bad request: ', error)
+            }
+        );
     }
 
-    let $input = $('input[type=text]').val();
-
-    $.ajax ({
-        url: 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=96179a5fe80fda98d6fccc841db6e2bc&units=imperial'
-    })
-    .then(
-        function(data) {
-            //if successful
-            weatherData = data;
-            render();
-            
-
-        }, 
-        function(error) {
-            console.log("bad request: ", error)
-        }
-    );
+    $('form').on('submit', handleGetData);
